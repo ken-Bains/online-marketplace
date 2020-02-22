a13ae4e77f558deecf34789921a5a7e28555072c
KB <nkbains10@gmail.com>
2020-02-22 12:15:21 -0800

    readme

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const SideNav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="navbar-brand">Online Marketplace</div>
@@ -15,4 +15,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+export default SideNav
